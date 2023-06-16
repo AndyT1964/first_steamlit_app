22055a3065c28133cdd90869203c3eaa5b629ef3
AndyT1964 <136822668+AndyT1964@users.noreply.github.com>
2023-06-16 16:16:57 +0200

    Update streamlit_app.py
    Added New Header

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,6 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Bolied Free-range Egg')
 streamlit.text('🥑🍞 Advacado on Toast')
 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+
