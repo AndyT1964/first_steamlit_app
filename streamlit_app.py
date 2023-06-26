96f4cbfc73d6f316f5e31b0c58c2516bbb7c76c4
AndyT1964 <136822668+AndyT1964@users.noreply.github.com>
2023-06-26 10:15:03 +0200

    Update streamlit_app.py
    Remove json formatted line

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,6 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
 
 # read the json data and formats the outcome to a tablular display
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
